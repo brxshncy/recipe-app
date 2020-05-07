52375f5e4a0a3363e44456d500b86d4c145f47b2
brxshncy <ggkidsbruce619@gmail.com>
2020-05-07 11:56:35 +0800

    Updates

---
+++ precache-manifest.c502cf4762394d67b933272ce1655b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffeb619d5244e0649fa8319cce550d3",
+    "url": "/brxshncy/recipe-app/index.html"
+  },
+  {
+    "revision": "19f91ade618d2d827b51",
+    "url": "/brxshncy/recipe-app/static/css/main.03434d17.chunk.css"
+  },
+  {
+    "revision": "be7d553cdce9173fff6e",
+    "url": "/brxshncy/recipe-app/static/js/2.4d20a349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brxshncy/recipe-app/static/js/2.4d20a349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f91ade618d2d827b51",
+    "url": "/brxshncy/recipe-app/static/js/main.9d3a7497.chunk.js"
+  },
+  {
+    "revision": "d17041215d92fc762817",
+    "url": "/brxshncy/recipe-app/static/js/runtime-main.983f2032.js"
+  }
+]);
